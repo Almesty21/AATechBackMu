9d747703ef51f451d3be39af8c170a9becd1724e
almesty21 <mihretabgirma21@gmail.com>
2023-09-20 10:55:57 -0300

    modify .env

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const songRoutes = require('./routes/song.routes');
 // console.log("song routes: ", songRoutes);
-
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = "mongodb+srv://Mihretab:Madonna21@cluster0.3wsehn9.mongodb.net/Addis_Software?retryWrites=true&w=majority" // Replace with your MongoDB URI
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
+const { MONGODB_URI } = process.env;
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/songs', songRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
